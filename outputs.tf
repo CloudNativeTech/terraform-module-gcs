6ee051782907b4f3676451acd2ca70f4f7cdf0f0
Pradeep Bhadani <pradeep.bhadani@gmail.com>
2020-01-12 11:52:14 +0000

    Add description to outputs.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "name" {
-  value = google_storage_bucket.bucket.name
+  description = "GCS bucket name."
+  value       = google_storage_bucket.bucket.name
 }
 
 output "self_link" {
-  value = google_storage_bucket.bucket.self_link
+  description = "URI of the GCS bucket."
+  value       = google_storage_bucket.bucket.self_link
 }
 
 output "url" {
-  value = google_storage_bucket.bucket.url
+  description = "The base URL of the bucket, in the format gs://<bucket-name>"
+  value       = google_storage_bucket.bucket.url
 }
